d18a5a2895f7cdc9e5a9af6b25f44f197bdf1ed6
franciscodvel <pacodvel@4b5d82362e5e>
2023-03-09 04:39:07 +0000

    beginning module 5, before unzip module 5

--- django-models-project-start/carved_rock/store/models.py
+++ django-models-project-start/carved_rock/store/models.py
@@ -8,11 +8,20 @@ class Product(models.Model):
     description = models.TextField(default="", blank=True)
     sku = models.CharField(unique=True,max_length=20,verbose_name="stock keeping unit")
 
+    def __str__(self):
+        return self.name
+
 class ProductImage(models.Model):
     image = models.ImageField()
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     name = models.CharField(max_length = 100)
     products = models.ManyToManyField('Product')
 
+    def __str__(self):
+        return self.name
