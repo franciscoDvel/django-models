3b040d4d050e24aa615eed3d17eb4e69b7215025
franciscodvel <pacodvel@4b5d82362e5e>
2023-03-16 04:57:22 +0000

    module: custimizing model behabiour

--- django-models-project-start/carved_rock/store/models.py
+++ django-models-project-start/carved_rock/store/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from decimal import Decimal
 
 
 class Product(models.Model):
@@ -8,13 +10,28 @@ class Product(models.Model):
     description = models.TextField(default="", blank=True)
     sku = models.CharField(verbose_name="Stock Keeping Unit", max_length=20, unique=True)
 
+    class Meta:
+        ordering = ['price']
+        constraints = [
+            models.CheckConstraint(check=models.Q(price__gte=0),name="price_not_negative")
+        ]
+    
+    def get_absolute_url(self):
+        return reverse("store:product-detail", kwargs={'pk': self.id})
+
+    @property
+    def vat(self):
+        return Decimal(.2) * self.price
+
+
+
     def __str__(self):
         return self.name
 
 
 class ProductImage(models.Model):
     image = models.ImageField()
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name="images")
 
     def __str__(self):
         return str(self.image)
@@ -23,6 +40,11 @@ class ProductImage(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100)
     products = models.ManyToManyField('Product')
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ['name']
+
 
     def __str__(self):
         return self.name
--- django-models-project-start/carved_rock/store/urls.py
+++ django-models-project-start/carved_rock/store/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import DetailView
+from .models import Product
 
 from .views import category_view
 
@@ -6,4 +8,6 @@ app_name = "store"
 
 urlpatterns = [
     path('category/<str:name>', category_view, name="category"),
+    path('product/<int:pk>', DetailView.as_view(model=Product), name="product-detail")
+
 ]
